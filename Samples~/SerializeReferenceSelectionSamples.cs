a2f4ebda4bd6ab19c257ae7d30cf89c2918711ec
godzzz <g0dzZz1lla@yandex.ru>
2023-08-18 01:34:56 +0400

    Some samples

---
+++ Samples~/SerializeReferenceSelectionSamples.cs
@@ -0,0 +1,31 @@
+using System;
+using Depra.SerializeReference.Selection.Runtime;
+using UnityEngine;
+
+namespace Depra.SerializeReference.Selection.Samples
+{
+	internal sealed class SerializeReferenceSelectionSamples : MonoBehaviour
+	{
+		[SubclassSelection]
+		[UnityEngine.SerializeReference] private IInterface _interface;
+
+		[SubclassSelection]
+		[UnityEngine.SerializeReference] private AbstractClass _abstractClass;
+
+		internal abstract class AbstractClass { }
+
+		[Serializable]
+		[AddTypeMenu(nameof(ClassImplementation))]
+		public sealed class ClassImplementation : AbstractClass { }
+
+		private interface IInterface { }
+
+		[Serializable]
+		[AddTypeMenu(nameof(ClassInterfaceImplementation))]
+		public sealed class ClassInterfaceImplementation : IInterface { }
+
+		[Serializable]
+		[AddTypeMenu(nameof(RecordInterfaceImplementation))]
+		public sealed record RecordInterfaceImplementation : IInterface { }
+	}
+}
