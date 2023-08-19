aa32d93fc3e388ae314602dcbd54b4678b8ecf9d
godzzz <g0dzZz1lla@yandex.ru>
2023-08-19 18:26:34 +0400

    Samples updated

--- Samples~/SerializeReferenceSelectionSamples.cs
+++ Samples~/SerializeReferenceSelectionSamples.cs
@@ -7,57 +7,54 @@ using UnityEngine;
 
 namespace Depra.SerializedReference.Dropdown.Samples
 {
-    internal sealed class SerializeReferenceSelectionSamples : MonoBehaviour
-    {
-        [UnityEngine.SerializeReference] [SubtypeMenu]
-        private ISampleCommand _command;
+	internal sealed class SerializeReferenceSelectionSamples : MonoBehaviour
+	{
+		[SubtypeMenu] [SerializeReference] private ISampleCommand _command;
+		[SubtypeMenu] [SerializeReference] private ISampleCommand[] _commands;
 
-        [UnityEngine.SerializeReference] [SubtypeMenu]
-        private ISampleCommand[] _commands;
+		private void Start()
+		{
+			_command?.Execute();
+			foreach (var command in _commands)
+			{
+				command?.Execute();
+			}
+		}
 
-        private void Start()
-        {
-            _command?.Execute();
-            foreach (var command in _commands)
-            {
-                command?.Execute();
-            }
-        }
+		[Serializable]
+		public sealed class NestedCommand : ISampleCommand
+		{
+			void ISampleCommand.Execute() => Debug.Log($"{nameof(ISampleCommand.Execute)} {nameof(NestedCommand)}");
+		}
+	}
 
-        [Serializable]
-        public sealed class NestedCommand : ISampleCommand
-        {
-            void ISampleCommand.Execute() => Debug.Log($"{nameof(ISampleCommand.Execute)} {nameof(NestedCommand)}");
-        }
-    }
+	internal interface ISampleCommand
+	{
+		void Execute();
+	}
 
-    internal interface ISampleCommand
-    {
-        void Execute();
-    }
+	[Serializable]
+	public sealed class ClassCommand : ISampleCommand
+	{
+		void ISampleCommand.Execute() => Debug.Log($"{nameof(ISampleCommand.Execute)} {nameof(ClassCommand)}");
+	}
 
-    [Serializable]
-    public sealed class ClassCommand : ISampleCommand
-    {
-        void ISampleCommand.Execute() => Debug.Log($"{nameof(ISampleCommand.Execute)} {nameof(ClassCommand)}");
-    }
+	[Serializable]
+	[SubtypeMenuAlias(nameof(CommandWithCustomTypeMenu))]
+	public sealed class CommandWithCustomTypeMenu : ISampleCommand
+	{
+		void ISampleCommand.Execute() => Debug.Log($"{nameof(ISampleCommand.Execute)} {nameof(CommandWithCustomTypeMenu)}");
+	}
 
-    [Serializable]
-    [SubtypeMenuAlias(nameof(CommandWithCustomTypeMenu))]
-    public sealed class CommandWithCustomTypeMenu : ISampleCommand
-    {
-        void ISampleCommand.Execute() => Debug.Log($"{nameof(ISampleCommand.Execute)} {nameof(CommandWithCustomTypeMenu)}");
-    }
+	[Serializable]
+	public readonly struct StructCommand : ISampleCommand
+	{
+		void ISampleCommand.Execute() => Debug.Log($"{nameof(ISampleCommand.Execute)} {nameof(StructCommand)}");
+	}
 
-    [Serializable]
-    public readonly struct StructCommand : ISampleCommand
-    {
-        void ISampleCommand.Execute() => Debug.Log($"{nameof(ISampleCommand.Execute)} {nameof(StructCommand)}");
-    }
-
-    [Serializable]
-    public sealed record RecordCommand : ISampleCommand
-    {
-        void ISampleCommand.Execute() => Debug.Log($"{nameof(ISampleCommand.Execute)} {nameof(RecordCommand)}");
-    }
+	[Serializable]
+	public sealed record RecordCommand : ISampleCommand
+	{
+		void ISampleCommand.Execute() => Debug.Log($"{nameof(ISampleCommand.Execute)} {nameof(RecordCommand)}");
+	}
 }
